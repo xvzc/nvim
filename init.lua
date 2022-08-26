948dde71c315ad59bde061e5d5315851c4725eb2
Kwanghoo Park <kwanghoopark@Kwanghoos-MacBook-Air-2.local>
2022-08-26 21:52:49 +0900

    feat: Add vim-lastplace plugin

--- init.lua
+++ init.lua
@@ -1,5 +1,9 @@
 vim.cmd('filetype plugin indent on')
 
+vim.g.lastplace_ignore = "gitcommit,gitrebase,svn,hgcommit"
+vim.g.lastplace_ignore_buftype = "quickfix,nofile,help"
+vim.g.lastplace_open_folds = 0
+
 require('plugins')
 require('clipboard')
 require('preferences')
