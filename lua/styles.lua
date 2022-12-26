e8b007c9cb51b939730879c43fb625372b3afc9c
xvzc <dev.kwanghoo@gmail.com>
2022-12-26 16:22:30 +0900

    fix: change line highlight

--- lua/styles.lua
+++ lua/styles.lua
@@ -6,7 +6,7 @@ hi Comment ctermfg=14 gui=italic guifg=#6b806e
 command! -bang -nargs=? -complete=dir HiGroup
 \ echo synIDattr(synID(line("."), col("."), 1), "name")
 
-hi CursorLine guibg=#373838
+hi CursorLine guibg=#2d364f
 hi PmenuSel blend=0
 ]])
 -- hi Cursor cterm=none gui=none guifg=#646466 ctermfg=black
