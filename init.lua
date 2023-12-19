f2fe9c94dc46cfc8ef5f885e1b6bf20a591bf473
xvzc <dev.kwanghoo@gmail.com>
2023-12-19 18:39:46 +0900

    feat: Add vim.loader

--- init.lua
+++ init.lua
@@ -1,5 +1,7 @@
 vim.cmd('filetype plugin indent on')
 
+vim.loader.enable()
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
