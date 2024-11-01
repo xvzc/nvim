426fd5839a6ec4bbfd543197c00944a5c8081787
xvzc <dev.kwanghoo@gmail.com>
2024-11-01 14:49:28 +0900

    update template for cpp

--- templates/cpp/algorithm.tmpl.cpp
+++ templates/cpp/algorithm.tmpl.cpp
@@ -6,7 +6,7 @@
 #define IF_LOCAL if constexpr (false)
 #endif
 
-#define debug IF_LOCAL std::cout << "[DEBUG] "
+#define debug IF_LOCAL std::cout << "[DEBUG]" << ' '
 #define endl '\n'
 #define fi first
 #define se second
@@ -17,9 +17,25 @@ typedef unsigned long long ull;
 
 using namespace std;
 
-/* Authored by xvzc, 2024-01-19 12:50:48 */
+template <typename X, typename Y>
+ostream& operator<<(ostream& os, const pair<X, Y>& p) {
+  os << "(" << p.first << ", " << p.second << ")";
+  return os;
+}
+
+template <class C, class T, class Container>
+basic_ostream<C, T>& operator<<(basic_ostream<C, T>& os, Container const& c) {
+  auto it = cbegin(c);
+  os << "{";
+  while (it != cend(c)) os << *it++ && it != cend(c) && os << ", ";
+  return os << "}";
+}
+
+/* ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ */
+
 int main() {
   ios_base::sync_with_stdio(false), cin.tie(0);
 
   return 0;
 }
+
