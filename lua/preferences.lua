f495f70495704c0504f33c0cd9aea54192235061
xvzc <dev.kwanghoo@gmail.com>
2024-11-11 15:07:38 +0900

    update

--- lua/preferences.lua
+++ lua/preferences.lua
@@ -50,7 +50,13 @@ vim.o.cursorline = true
 
 vim.o.shortmess = vim.o.shortmess .. "c"
 vim.o.mouse = "a"
+
 vim.o.viminfo = "'100,<1000,s100,h"
+-- '100 Marks will be remembered for the last 100 edited files.
+-- <1000 Limits the number of lines saved for each register to 1000 lines; 
+-- if a register contains more than 1000 lines, only the first 1000 lines are saved.
+-- s100 Registers with more than 100 KB of text are skipped.
+-- h Disables search highlighting when Vim starts.
 
 vim.o.sol = false
 vim.o.showmode = false
