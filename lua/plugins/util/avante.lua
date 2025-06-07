eccf73a083ae7102554903750f7de936786be26f
xvzc <me@xvzc.dev>
2025-06-07 23:18:22 +0900

    chore: fix avante

--- lua/plugins/util/avante.lua
+++ lua/plugins/util/avante.lua
@@ -1,11 +1,12 @@
 local avante = require("avante")
 avante.setup({
-  openai = {
-    provider = "openai",
-    endpoint = "https://api.openai.com/v1",
-    model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
-    timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
-    -- temperature = 0,
-    -- max_completion_tokens = 8192,
+  providers = {
+    openai = {
+      endpoint = "https://api.openai.com/v1",
+      model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
+      timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
+      -- temperature = 0,
+      -- max_completion_tokens = 8192,
+    },
   },
 })
