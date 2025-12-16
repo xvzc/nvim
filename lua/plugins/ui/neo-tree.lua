3089f61768129bb5169d4c67dcecb568685c2e28
xvzc <me@xvzc.dev>
2025-12-16 17:34:29 +0900

    feat(neo-tree): add reveal option when opening

--- lua/plugins/ui/neo-tree.lua
+++ lua/plugins/ui/neo-tree.lua
@@ -3,7 +3,7 @@ local keys = {
     mode = { "n" },
     "<leader>e",
     function()
-      require("neo-tree.command").execute({ toggle = true })
+      require("neo-tree.command").execute({ toggle = true, reveal = true })
     end,
     silent = true,
   },
